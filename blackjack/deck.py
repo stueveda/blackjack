0eec4b97c4f24f091269d2029934f42c16861428
Dan S <stueveda@gmail.com>
2023-08-14 17:21:07 -0500

    remove tests from deck file

--- blackjack/deck.py
+++ blackjack/deck.py
@@ -22,25 +22,3 @@ class Deck:
 
     def add_cards(self, new_cards):
         self.cards.extend(new_cards)
-
-
-if __name__ == '__main__':
-    d = Deck()
-
-    assert d.count() == 0
-    assert not d.peek()
-    assert not d.draw()
-    
-    d.add_cards('A')
-    assert d.count() == 1
-    assert d.peek() == 'A'
-    assert d.draw() == 'A'
-    assert d.count() == 0
-
-    d.add_cards(constants.STANDARD_DECK)
-    assert d.count() == 52
-    assert d.peek()
-    assert d.draw()
-    assert d.count() == 51
-    d.empty_deck()
-    assert d.count() == 0
